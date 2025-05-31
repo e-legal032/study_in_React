a669646cb6b701c57a1de5fc3353847087c73910
e-legal032 <e.legal032@gmail.com>
2025-05-31 22:14:38 +0000

    "Contador" component, Hooks/useState

--- react_FCC_projs/contador-de-clics/src/App.js
+++ react_FCC_projs/contador-de-clics/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import Contador from './componentes/Contador';
+import Boton from './componentes/Boton';
+import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
+import { useState } from 'react';
+
 
 function App() {
+  const [numClics, setnumClics]= useState(0);
+
+  const manejarClic = () =>{
+    setnumClics(numClics + 1);
+  }
+
+  const reiniciarContador = () =>{
+    setnumClics(0);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='freecodecamp-logo-contenedor'>
+        <img
+          className='freecodecamp-logo'
+          src={freeCodeCampLogo}
+          alt='Logo de freeCode Camp'/>
+      </div>
+      <div className='contenedor-principal'>
+        <Contador
+          numClics={numClics} />
+        <Boton
+          texto='Click'
+          esBotonDeClic={true}
+          manejarClic={manejarClic} />
+        <Boton
+          texto='Reiniciar'
+          esBotonDeClic={false}
+          manejarClic={reiniciarContador} />
+      </div>
     </div>
-  );
+  );  
 }
 
 export default App;
